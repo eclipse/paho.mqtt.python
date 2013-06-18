3c1f77926ea4864b38c68712875362b465c7d3b3
Roger Light <roger@atchoo.org>
2013-06-18 23:12:19 +0100

    Change licensing BSD -> dual EPL/EDL.
    --HG--
    rename : LICENSE.txt => epl-v10

--- src/paho/mqtt/client.py
+++ src/paho/mqtt/client.py
@@ -1,36 +1,16 @@
-# Copyright (c) 2012 Roger Light <roger@atchoo.org>
-# All rights reserved.
-# 
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# 
-# 1. Redistributions of source code must retain the above copyright notice,
-#    this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of mosquitto nor the names of its
-#    contributors may be used to endorse or promote products derived from
-#    this software without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+# Copyright (c) 2012-2013 Roger Light <roger@atchoo.org>
 #
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# and Eclipse Distribution License v1.0 which accompany this distribution. 
 #
-# This product includes software developed by the OpenSSL Project for use in
-# the OpenSSL Toolkit. (http://www.openssl.org/)
-# This product includes cryptographic software written by Eric Young
-# (eay@cryptsoft.com)
-# This product includes software written by Tim Hudson (tjh@cryptsoft.com)
+# The Eclipse Public License is available at 
+#    http://www.eclipse.org/legal/epl-v10.html
+# and the Eclipse Distribution License is available at 
+#   http://www.eclipse.org/org/documents/edl-v10.php.
+#
+# Contributors:
+#    Roger Light - initial API and implementation
 
 """
 This is an MQTT v3.1 client module. MQTT is a lightweight pub/sub messaging
