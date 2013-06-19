05e951af7e5c9564818a17184271fb4edc9734ee
Roger Light <roger@atchoo.org>
2013-06-19 10:18:46 +0100

    Fix license classifiers and version number.

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from sys import version
 
 from distutils.core import setup
-setup(name='paho`',
-	version='0.9.0',
+setup(name='paho',
+	version='0.7.0',
 	description='MQTT version 3.1 client class',
 	author='Roger Light',
 	author_email='roger@atchoo.org',
 	url='http://eclipse.org/paho',
-	# FIXME license='Eclipse Public License',
+	license='Eclipse Public License v1.0 / Eclipse Distribution License v1.0',
     package_dir={'': 'src'},
     packages=['paho', 'paho.mqtt'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        # FIXME 'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
