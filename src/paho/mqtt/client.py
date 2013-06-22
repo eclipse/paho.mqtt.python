d5c238872a0ca313df0f5bdab99b210e9be805f3
Roger Light <roger@atchoo.org>
2013-06-22 22:11:47 +0100

    Auto generate client id if it is None as well as "".

--- src/paho/mqtt/client.py
+++ src/paho/mqtt/client.py
@@ -377,7 +377,7 @@ class Client:
         self._message_retry = 20
         self._last_retry_check = 0
         self._clean_session = clean_session
-        if client_id == "":
+        if client_id == "" or client_id == None:
             self._client_id = "paho/" + "".join(random.choice("0123456789ADCDEF") for x in range(23-5))
         else:
             self._client_id = client_id
