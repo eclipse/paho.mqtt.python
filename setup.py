dd6617ffb48700580ac851aeaf7ed789983fae28
Roger A. Light <roger@atchoo.org>
2014-05-12 20:57:08 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from sys import version
 
 from distutils.core import setup
 setup(name='paho-mqtt',
-	version='0.9',
+	version='0.9.1',
 	description='MQTT version 3.1/3.1.1 client class',
 	author='Roger Light',
 	author_email='roger@atchoo.org',
