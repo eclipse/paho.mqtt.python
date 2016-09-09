33b7c95df3df330ab2ffb7b325e94164db1159fd
James Myatt <james@jamesmyatt.co.uk>
2016-09-09 20:53:22 +0100

    Update examples added since PR#98 (#109)
    Signed-off-by: James Myatt <james@jamesmyatt.co.uk>

--- examples/client_logger.py
+++ examples/client_logger.py
@@ -1,38 +1,24 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 # Copyright (c) 2016 James Myatt <james@jamesmyatt.co.uk>
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Distribution License v1.0
-# which accompanies this distribution. 
+# which accompanies this distribution.
 #
-# The Eclipse Distribution License is available at 
+# The Eclipse Distribution License is available at
 #   http://www.eclipse.org/org/documents/edl-v10.php.
 #
 # Contributors:
 #    James Myatt - initial implementation
 
-# This shows a simple example of standard logging with an MQTT subscriber.
+# This shows a simple example of standard logging with an MQTT subscriber client.
 
-import sys
+import context  # Ensures paho is in PYTHONPATH
+import paho.mqtt.client as mqtt
+
 import logging
-
-try:
-    import paho.mqtt.client as mqtt
-except ImportError:
-    # This part is only required to run the example from within the examples
-    # directory when the module itself is not installed.
-    #
-    # If you have the module installed, just use "import paho.mqtt.client"
-    import os
-    import inspect
-
-    cmd_subfolder = os.path.realpath(
-        os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0], "../src")))
-    if cmd_subfolder not in sys.path:
-        sys.path.insert(0, cmd_subfolder)
-    import paho.mqtt.client as mqtt
-
 logging.basicConfig(level=logging.DEBUG)
 
 # If you want to use a specific client id, use
--- examples/client_pub-wait.py
+++ examples/client_pub-wait.py
@@ -15,39 +15,32 @@
 # Copyright (c) 2010,2011 Roger Light <roger@atchoo.org>
 # All rights reserved.
 
-# This shows a simple example of an MQTT subscriber.
+# This shows a simple example of waiting for a message to be published.
 
-import sys
-import time
-try:
-    import paho.mqtt.client as mqtt
-except ImportError:
-    # This part is only required to run the example from within the examples
-    # directory when the module itself is not installed.
-    #
-    # If you have the module installed, just use "import paho.mqtt.client"
-    import os
-    import inspect
-    cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0], "../src")))
-    if cmd_subfolder not in sys.path:
-        sys.path.insert(0, cmd_subfolder)
-    import paho.mqtt.client as mqtt
+import context  # Ensures paho is in PYTHONPATH
+import paho.mqtt.client as mqtt
+
 
 def on_connect(mqttc, obj, flags, rc):
-    print("rc: "+str(rc))
+    print("rc: " + str(rc))
+
 
 def on_message(mqttc, obj, msg):
-    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
 
 def on_publish(mqttc, obj, mid):
-    print("mid: "+str(mid))
+    print("mid: " + str(mid))
     pass
 
+
 def on_subscribe(mqttc, obj, mid, granted_qos):
-    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
 
 def on_log(mqttc, obj, level, string):
     print(string)
+
 
 # If you want to use a specific client id, use
 # mqttc = mqtt.Client("client-id")
@@ -59,7 +52,7 @@ mqttc.on_connect = on_connect
 mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
 # Uncomment to enable debug messages
-#mqttc.on_log = on_log
+# mqttc.on_log = on_log
 mqttc.connect("localhost", 1883, 60)
 
 mqttc.loop_start()
