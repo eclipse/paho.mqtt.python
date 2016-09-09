a7907901a567f2e1b353b9461c7ec602e6e54354
James Myatt <james@jamesmyatt.co.uk>
2016-09-09 14:52:20 +0100

    Update examples added since PR#98
    Signed-off-by: James Myatt <james@jamesmyatt.co.uk>

--- examples/client_session_present.py
+++ examples/client_session_present.py
@@ -17,20 +17,9 @@
 
 # This demonstrates the session present flag when connecting.
 
-import sys
-try:
-    import paho.mqtt.client as mqtt
-except ImportError:
-    # This part is only required to run the example from within the examples
-    # directory when the module itself is not installed.
-    #
-    # If you have the module installed, just use "import paho.mqtt.client"
-    import os
-    import inspect
-    cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../src")))
-    if cmd_subfolder not in sys.path:
-        sys.path.insert(0, cmd_subfolder)
-    import paho.mqtt.client as mqtt
+import context  # Ensures paho is in PYTHONPATH
+import paho.mqtt.client as mqtt
+
 
 def on_connect(mqttc, obj, flags, rc):
     if obj == 0:
@@ -39,23 +28,26 @@ def on_connect(mqttc, obj, flags, rc):
         print("Second connection:")
     elif obj == 2:
         print("Third connection (with clean session=True):")
-    print("    Session present: "+str(flags['session present']))
-    print("    Connection result: "+str(rc))
+    print("    Session present: " + str(flags['session present']))
+    print("    Connection result: " + str(rc))
     mqttc.disconnect()
 
+
 def on_disconnect(mqttc, obj, rc):
-    mqttc.user_data_set(obj+1)
+    mqttc.user_data_set(obj + 1)
     if obj == 0:
         mqttc.reconnect()
 
+
 def on_log(mqttc, obj, level, string):
     print(string)
+
 
 mqttc = mqtt.Client(client_id="asdfj", clean_session=False)
 mqttc.on_connect = on_connect
 mqttc.on_disconnect = on_disconnect
 # Uncomment to enable debug messages
-#mqttc.on_log = on_log
+# mqttc.on_log = on_log
 mqttc.user_data_set(0)
 mqttc.connect("test.mosquitto.org", 1883, 60)
 
