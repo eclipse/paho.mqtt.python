5f29f3705c33f652b734ee0efea0b14819ee4f31
James Myatt <james@jamesmyatt.co.uk>
2017-04-24 14:05:58 +0100

    Remove version "constants"

--- src/paho/mqtt/__init__.py
+++ src/paho/mqtt/__init__.py
@@ -1,10 +1,5 @@
 __version__ = "1.3.0.dev0"
 
-VERSION_MAJOR = 1
-VERSION_MINOR = 3
-VERSION_REVISION = 0
-VERSION_NUMBER = (VERSION_MAJOR * 1000000 + VERSION_MINOR * 1000 + VERSION_REVISION)
-
 
 class MQTTException(Exception):
     def __init__(self, *args, **kwargs):
