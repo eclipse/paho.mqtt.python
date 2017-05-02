8e2a27ff7aa619566b11734da0be34daad9dab1d
Michael Boulton <boulton@zoetrope.io>
2017-05-02 11:16:02 +0100

    Fix unit test to work with python 2

--- tests/test_websockets.py
+++ tests/test_websockets.py
@@ -27,33 +27,43 @@ class TestHeaders(object):
         ]
 
         def iter_response():
-            for i in "\r\n".join(response):
+            for i in "\r\n".join(response).encode("utf8"):
+                yield i
+
+            for i in "\r\n".encode("utf8"):
                 yield i
 
         it = iter_response()
 
         def fakerecv(*args):
-            return next(it)
+            if sys.version_info < (3, 0):
+                return next(it)
+            else:
+                return bytes([next(it)])
 
         mocksock = Mock(
             spec_set=socket.socket,
             recv=fakerecv,
             send=Mock(),
         )
-        host = "testhost.com"
-        port = 1234
-        path = "/mqtt"
-        extra_headers = None
-        is_ssl = True
+
+        wargs = {
+            "host": "testhost.com",
+            "port": 1234,
+            "path": "/mqtt",
+            "extra_headers": None,
+            "is_ssl": True,
+            "socket": mocksock,
+        }
 
         with pytest.raises(WebsocketConnectionError) as exc:
-            w = WebsocketWrapper(mocksock, host, port, is_ssl, path, extra_headers)
+            WebsocketWrapper(**wargs)
 
         # We're not creating the response hash properly so it should raise this
         # error
         assert str(exc.value) == "WebSocket handshake error, invalid secret key"
 
-        expected_sent = [i.format(**locals()) for i in [
+        expected_sent = [i.format(**wargs) for i in [
             "GET {path:s} HTTP/1.1",
             "Host: {host:s}",
             "Upgrade: websocket",
@@ -67,4 +77,4 @@ class TestHeaders(object):
         assert mocksock.send.call_count == 1
 
         for i in expected_sent:
-            assert i in mocksock.send.call_args[0][0]
+            assert i in mocksock.send.call_args[0][0].decode("utf8")
