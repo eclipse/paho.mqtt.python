a91e6775ebb970ca91cf282ca96475673bff9278
Ian Craggs <icraggs@uk.ibm.com>
2019-06-11 18:14:19 +0100

    Start test broker in tox only environment.  Add Python 3.7 testing.

--- test-broker-run.sh
+++ test-broker-run.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/eclipse/paho.mqtt.testing.git
 cd paho.mqtt.testing/interoperability
-python3 startbroker.py -c localhost_testing.conf &
+python3 startbroker.py -c localhost_testing.conf > test_broker.out 2> test_broker.err &
 cd ../..
