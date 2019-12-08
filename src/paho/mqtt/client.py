5f5166fa24c6a57c7cd5aea48cf1d24029127b6f
Josiah Witheford <josiah.witheford@gmail.com>
2019-12-08 17:24:50 +1300

    Add context manager back
    Signed-off-by: Josiah Witheford <josiah.witheford@gmail.com>

--- src/paho/mqtt/client.py
+++ src/paho/mqtt/client.py
@@ -3412,15 +3412,16 @@ class Client(object):
 
             if topic is not None:
                 for callback in self._on_message_filtered.iter_match(message.topic):
-                    try:
-                        callback(self, self._userdata, message)
-                    except Exception as err:
-                        self._easy_log(
-                            MQTT_LOG_ERR,
-                            'Caught exception in user defined callback function %s: %s',
-                            callback.__name__,
-                            err
-                        )
+                    with self._in_callback_mutex:
+                        try:
+                            callback(self, self._userdata, message)
+                        except Exception as err:
+                            self._easy_log(
+                                MQTT_LOG_ERR,
+                                'Caught exception in user defined callback function %s: %s',
+                                callback.__name__,
+                                err
+                            )
                     matched = True
 
             if matched == False and self.on_message:
