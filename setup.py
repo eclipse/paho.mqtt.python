77e2ccacda5d925608bdf2f620eed8d0325b6a89
Peter Silva <peter@bsqt.homeip.net>
2023-10-17 09:07:26 -0400

    address #713: remove claims of python2 support, as it is no longer supported.

--- setup.py
+++ setup.py
@@ -44,8 +44,6 @@ setup(
         'Operating System :: POSIX',
         'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
