8c398c04d2539eb481df9c22dff8ce8070e704f7
Pierre Fersing <pierre.fersing@bleemeo.com>
2024-01-21 16:11:26 +0100

    Add __repr__ to ReasonCodes
    This allow to have better print for a list of ReasonCodes, for
    example in on_subscribe callback.

--- src/paho/mqtt/reasoncodes.py
+++ src/paho/mqtt/reasoncodes.py
@@ -189,6 +189,14 @@ class ReasonCode:
             return self.value < other.value
         return NotImplemented
 
+    def __repr__(self):
+        try:
+            packet_name = PacketTypes.Names[self.packetType]
+        except IndexError:
+            packet_name = "Unknown"
+
+        return f"ReasonCode({packet_name}, {self.getName()!r})"
+
     def __str__(self):
         return self.getName()
 
