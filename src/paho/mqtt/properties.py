d22ab65a32012c43a976be4d9fdc5c43fac08ff9
Matthias Urlichs <matthias@urlichs.de>
2024-05-27 13:42:43 +0200

    dict.keys() is an iterator in Python 3
    Thus (a) "foo in dict.keys()" is better written as "foo in dict"
    and (b) "print(foo.keys())" won_t do what you want

--- src/paho/mqtt/properties.py
+++ src/paho/mqtt/properties.py
@@ -242,7 +242,7 @@ class Properties:
     def getIdentFromName(self, compressedName):
         # return the identifier corresponding to the property name
         result = -1
-        for name in self.names.keys():
+        for name in self.names:
             if compressedName == name.replace(' ', ''):
                 result = self.names[name]
                 break
@@ -255,9 +255,9 @@ class Properties:
             object.__setattr__(self, name, value)
         else:
             # the name could have spaces in, or not.  Remove spaces before assignment
-            if name not in [aname.replace(' ', '') for aname in self.names.keys()]:
+            if name not in [aname.replace(' ', '') for aname in self.names]:
                 raise MQTTException(
-                    f"Property name must be one of {self.names.keys()}")
+                    f"Property name must be one of {list(self.names)}")
             # check that this attribute applies to the packet type
             if self.packetType not in self.properties[self.getIdentFromName(name)][1]:
                 raise MQTTException(f"Property {name} does not apply to packet type {PacketTypes.Names[self.packetType]}")
@@ -292,7 +292,7 @@ class Properties:
     def __str__(self):
         buffer = "["
         first = True
-        for name in self.names.keys():
+        for name in self.names:
             compressedName = name.replace(' ', '')
             if hasattr(self, compressedName):
                 if not first:
@@ -304,7 +304,7 @@ class Properties:
 
     def json(self):
         data = {}
-        for name in self.names.keys():
+        for name in self.names:
             compressedName = name.replace(' ', '')
             if hasattr(self, compressedName):
                 val = getattr(self, compressedName)
@@ -316,7 +316,7 @@ class Properties:
 
     def isEmpty(self):
         rc = True
-        for name in self.names.keys():
+        for name in self.names:
             compressedName = name.replace(' ', '')
             if hasattr(self, compressedName):
                 rc = False
@@ -324,7 +324,7 @@ class Properties:
         return rc
 
     def clear(self):
-        for name in self.names.keys():
+        for name in self.names:
             compressedName = name.replace(' ', '')
             if hasattr(self, compressedName):
                 delattr(self, compressedName)
@@ -351,7 +351,7 @@ class Properties:
     def pack(self):
         # serialize properties into buffer for sending over network
         buffer = b""
-        for name in self.names.keys():
+        for name in self.names:
             compressedName = name.replace(' ', '')
             if hasattr(self, compressedName):
                 identifier = self.getIdentFromName(compressedName)
